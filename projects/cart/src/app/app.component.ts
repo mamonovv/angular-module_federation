1a6a3e7c7c28369c02061ac1421e95c282f61ab6
RUBY\vmamonov <vmamonov@kept.ru>
2023-11-15 17:38:31 +0700

    finish cart logic

--- projects/cart/src/app/app.component.ts
+++ projects/cart/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthLibService } from 'auth-lib';
 
 @Component({
@@ -9,12 +10,31 @@ import { AuthLibService } from 'auth-lib';
 export class AppComponent implements OnInit {
   title = 'cart';
 
+  items: any[] = [];
+
+  get itemsTotal() {
+    return this.items.reduce((a,v) => a + v.quantity * v.price, 0)
+  }
+
   constructor(
-    public auth: AuthLibService
+    public auth: AuthLibService,
+    private router: Router
   ) {
 
   }
 
   ngOnInit(): void {
+    if (!this.auth.isLoggedIn) {
+      this.router.navigate(['/home'])
+      return
+    }
+
+    this.auth.cart$.subscribe((cart: any) => {
+      this.items = cart?.cartItems ?? []
+    })
+  }
+
+  clearCart() {
+    this.auth.clearCart();
   }
 }
